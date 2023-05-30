c3f021a289ce6137ad6dde61b5bf983f8306b29c
misikira <misikirteka@gmail.com>
2023-05-30 17:27:00 +0300

    display the movement on home screen

--- starter/script.js
+++ starter/script.js
@@ -114,3 +114,19 @@ movements.forEach(function (mov,i,arr)
 
 // console.log(arr.at(-1))
 
+const displayMovents=function (movements) {
+  containerMovements.innerHTML='';
+  movements.forEach(function (mov,i) {
+    const type=mov > 0?'deposit':'withdrawal';
+  
+  const html=`
+  <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i +1}${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>`
+ 
+
+containerMovements.insertAdjacentHTML('afterbegin',html)
+});     
+};
+displayMovents(account1.movements)
